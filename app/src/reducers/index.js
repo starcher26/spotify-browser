f8b82a4f13a5c3431a5d7c8b5e03c231ee10e8d0
unknown <mounirk@digital-effervescence.com>
2018-09-19 01:10:20 +0200

    Add reducer/index.js containing reducers list

--- app/src/reducers/index.js
+++ app/src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from "redux";
-import tokenReducer from './tokenReducer';
+import tokenReducer from "./tokenReducer";
+import artistReducer from "./artistReducer";
+import albumReducer from "./albumReducer";
+import uiReducer from "./uiReducer";
 
 export default combineReducers({
   tokenReducer,
+  artistReducer,
+  albumReducer,
+  uiReducer
 });
